5f3a01186a49543f41c9019cff4ec0eae4fe253a
Mike Esposito <mikeesposito6852@gmail.com>
2020-05-18 17:49:23 -0700

    Refactor and add comments

--- substrings.rb
+++ substrings.rb
@@ -6,14 +6,15 @@ def update_output_hash(word, output_hash)
   output_hash
 end
 
-# checks for occurrence of a word in the input text
-def compare_strings(input_text, dictionary_word, output_hash)
+# checks for occurrence of a dictionary word in the input text
+# calls update_output_hash if it finds a word
+def compare_strings(input_text, dictionary_word)
   for i in 0..input_text.length - 1 do
     if input_text.length - i >= dictionary_word.length
       j = i + dictionary_word.length - 1
-      temp_array = input_text.slice(i..j)
-      if temp_array.eql?(dictionary_word)
-        @output_hash = update_output_hash(temp_array, output_hash)
+      arr = input_text.slice(i..j)
+      if arr.eql?(dictionary_word)
+        @output_hash = update_output_hash(arr, @output_hash)
       end
     end
   end
@@ -23,7 +24,7 @@ end
 def substrings(input_text, dictionary)
   input_text.downcase!
   dictionary.each do |word|
-    compare_strings(input_text, word, @output_hash)
+    compare_strings(input_text, word)
   end
   return @output_hash
 end
